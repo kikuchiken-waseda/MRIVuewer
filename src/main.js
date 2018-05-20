a4f5d9bab50f59fa323762ee8608a7abaa064c7f
qh73xe <takuya.waseda.1119@gmail.com>
2018-05-21 05:32:46 +0900

    画像アノテーションを可能にした.

--- src/main.js
+++ src/main.js
@@ -25,7 +25,10 @@ Vue.component(
           dragSelection: {
             slop: 5
           },
-          color: 'rgba(255,183,77, 0.3)'
+          color: 'rgba(255,183,77, 0.3)',
+          data: {
+            type: 'region'
+          }
         },
         spectrogramSetting: {
           container: '#wave-spectrogram',
@@ -41,6 +44,7 @@ Vue.component(
           progressColor: '#999',
           cursorColor: '#999'
         },
+        points: [],
         wavesurfer: null // wavesurfer クラス
       }
     },
@@ -95,6 +99,9 @@ Vue.component(
     },
     methods: {
       play: function (event) {
+        if (event.type === 'keyup') {
+          event.preventDefault()
+        }
         const video = document.getElementById('nowVideo')
         this.wavesurfer.playPause()
         if (video.paused) {
@@ -128,60 +135,7 @@ Vue.component(
           this.playBtnIcon = 'pause'
         }
       },
-      // region 操作
-      regionPlay: function (event) {
-        let currentTime = null
-        currentTime = this.wavesurfer.getCurrentTime()
-        for (const key in this.wavesurfer.regions.list) {
-          const region = this.wavesurfer.regions.list[key]
-          if (currentTime > region.start && currentTime < region.end) {
-            console.log(region)
-          }
-        }
-      },
-      regionUpdate: function (event) {
-        // 表示 regions を更新する
-        console.log(event)
-
-        // 現状の  id 一覧を取得
-        const regionList = this.regionSettiong.regions
-        const registeredIds = []
-        for (const i in regionList) {
-          registeredIds.push(regionList[i].id)
-        }
-
-        const regions = this.wavesurfer.regions.list
-        for (const key in regions) {
-          const region = regions[key]
-          if (registeredIds.indexOf(region.id) === -1) {
-            // 新規 region が波形レイヤーに存在する場合のみ追加
-            const duration = region.end - regions.start
-            const item = {
-              start: region.start,
-              end: region.end,
-              data: {
-                duration: duration,
-                contents: null
-              },
-              id: region.id
-            }
-            regionList.push(item)
-          } else {
-            // 開始, 終了時刻は異なる場合は反映
-            const index = registeredIds.indexOf(region.id)
-            const oldRegion = regionList[index]
-            if (oldRegion.start !== region.start || oldRegion.end !== region.end) {
-              regionList[index].start = region.start
-              regionList[index].end = region.end
-            }
-          }
-          regionList.sort(function (a, b) {
-            if (a.start < b.start) return -1
-            if (a.start > b.start) return 1
-            return 0
-          })
-        }
-      },
+      // 音声波形操作
       syncVideo: function (event) {
         const by = parseFloat(this.by)
         this.currentTime = this.wavesurfer.getCurrentTime()
@@ -210,6 +164,123 @@ Vue.component(
           this.wavesurfer = WaveSurfer.create(setting)
           this.wavesurfer.load(this.url)
           this.playBtnIcon = 'play_arrow'
+        })
+      },
+      // point 操作
+      pointAdd: function (event) {
+        /**
+         * HTML: id="waveform" 要素を ctrl+click した場合に
+         * waveform の現在値の情報を取得し, this.points に追加します.
+         */
+
+        const range = 0.005
+        const currentTime = this.wavesurfer.getCurrentTime()
+        const currentFrame = Math.floor(currentTime * this.fps)
+        if (currentTime !== 0) {
+          const item = {
+            start: currentTime - range,
+            end: currentTime - range,
+            data: {
+              time: currentTime,
+              frame: currentFrame,
+              type: 'point',
+              contents: null
+            },
+            color: 'rgba(103, 58, 183, 0.5)',
+            resize: false,
+            id: 'point_' + (this.points.length + 1)
+          }
+          this.points.push(item)
+
+          // 指定時刻の画像をキャンバスに追加
+          const video = document.getElementById('nowVideo')
+          const canvas = document.getElementById('video-canvas')
+          video.currentTime = item.data.time
+          canvas.width = video.offsetWidth * 5
+          canvas.height = video.offsetHeight * 5
+          const ctx = canvas.getContext('2d')
+          ctx.drawImage(video, 0, 0, video.offsetWidth * 5, video.offsetHeight * 5)
+          // wavesurfer に登録
+          this.wavesurfer.addRegion(item)
+          this.wavesurfer.fireEvent('region-updated', this.wavesurfer)
+        }
+      },
+      // canvas 操作
+      markAdd: function (event) {
+        const canvas = document.getElementById('video-canvas')
+        const ctx = canvas.getContext('2d')
+        const rect = canvas.getBoundingClientRect()
+        const x = event.clientX - rect.left
+        const y = event.clientY - rect.top
+        const color = 'rgba(244,81,30 ,1)'
+        const pointSize = 5
+
+        ctx.fillStyle = color
+        ctx.beginPath()
+        ctx.arc(x, y, pointSize, 0, Math.PI * 2, false)
+        ctx.fill()
+      },
+      // region 操作
+      regionPlay: function (event) {
+        let currentTime = null
+        currentTime = this.wavesurfer.getCurrentTime()
+        for (const key in this.wavesurfer.regions.list) {
+          const region = this.wavesurfer.regions.list[key]
+          if (currentTime > region.start && currentTime < region.end) {
+            console.log(region)
+          }
+        }
+      },
+      regionUpdate: function (event) {
+        /**
+         * レギオン作成時にアノテーションリストを更新
+         *
+         * detail:
+         *   waveform にある regions をすべて確認し,
+         *   this.regionSettiong.regions を更新します.
+         * event: mouseup
+         * target: id=waveform
+         */
+        const regionList = this.regionSettiong.regions
+        const registeredIds = []
+        for (const i in regionList) {
+          // 現状の  id 一覧を取得
+          registeredIds.push(regionList[i].id)
+        }
+        const regions = this.wavesurfer.regions.list
+        for (const key in regions) {
+          const region = regions[key]
+          if (region.data.type !== undefined && region.data.type === 'region') {
+            if (registeredIds.indexOf(region.id) === -1) {
+              // 新規 region が波形レイヤーに存在する場合のみ追加
+              const duration = region.end - regions.start
+              const item = {
+                start: region.start,
+                end: region.end,
+                color: region.color,
+                data: {
+                  type: 'region',
+                  duration: duration,
+                  contents: null
+                },
+                id: region.id
+              }
+              regionList.push(item)
+            } else {
+              // 開始, 終了時刻は異なる場合は反映
+              const index = registeredIds.indexOf(region.id)
+              const oldRegion = regionList[index]
+              if (oldRegion.start !== region.start || oldRegion.end !== region.end) {
+                regionList[index].start = region.start
+                regionList[index].end = region.end
+              }
+            }
+          }
+        }
+        regionList.sort(function (a, b) {
+          if (a.start < b.start) return -1
+          if (a.start > b.start) return 1
+          return 0
         })
       }
     },
@@ -345,10 +416,16 @@ Vue.component(
               <v-container>
                 <div id="wave-spectrogram"></div>
                 <div id="wave-timeline"></div>
-                <div id="waveform"
+                <div id="waveform" tabindex="1"
                   v-on:mouseup.exact="regionUpdate"
                   v-on:click="syncVideo"
-                  @click.ctrl.exact="regionPlay">
+                  @click.ctrl.exact="pointAdd"
+                  @click.alt.exact="regionPlay"
+
+                  @keyup.space="play"
+                  @keyup.ctrl.space="reegionPlay"
+                  @keyup.enter="pointAdd"
+                  >
                 </div>
               </v-container>
             </v-card>
@@ -386,9 +463,6 @@ Vue.component(
                       </v-list-tile-sub-title>
                     </v-list-tile-content>
                     <v-list-tile-action>
-                      <v-list-tile-action-text>
-                        {{ (item.end - item.start).toFixed(3) }} sec
-                      </v-list-tile-action-text>
                       <v-btn outline small icon color="indigo">
                         <v-icon>play_arrow</v-icon>
                       </v-btn>
@@ -420,8 +494,8 @@ Vue.component(
                   <v-icon>view_module</v-icon>
                 </v-btn>
               </v-toolbar>
-              <v-list three-line subheader v-if="regionSettiong.regions.length !== 0">
-                <template v-for="(item, index) in regionSettiong.regions">
+              <v-list three-line subheader v-if="points.length !== 0">
+                <template v-for="(item, index) in points">
                   <v-list-tile :key="item.id">
                     <v-list-tile-content>
                       <v-list-tile-sub-title>
@@ -432,18 +506,15 @@ Vue.component(
                         </v-text-field>
                       </v-list-tile-sub-title>
                       <v-list-tile-sub-title>
-                        START: {{ item.start.toFixed(3) }} sec
+                        Time: {{ item.data.time.toFixed(3) }} sec
                       </v-list-tile-sub-title>
                       <v-list-tile-sub-title>
-                        END: {{ item.end.toFixed(3) }} sec
+                        Frame: {{ item.data.frame.toFixed(0) }}
                       </v-list-tile-sub-title>
                     </v-list-tile-content>
                     <v-list-tile-action>
-                      <v-list-tile-action-text>
-                        {{ (item.end - item.start).toFixed(3) }} sec
-                      </v-list-tile-action-text>
                       <v-btn outline small icon color="indigo">
-                        <v-icon>play_arrow</v-icon>
+                        <v-icon>edit</v-icon>
                       </v-btn>
                       <v-btn outline small icon color="indigo">
                         <v-icon>delete_outline</v-icon>
@@ -456,13 +527,29 @@ Vue.component(
               <v-card-title v-else>
                 <v-container align-center>
                   <v-alert :value="true" type="warning">
-                    No Regions
+                    No Points
                   </v-alert>
                 </v-container>
               </v-card-title>
             </v-card>
           </v-container>
+        </v-flex>
 
+        <v-flex xs8 v-if="isReady">
+          <v-container>
+            <v-card>
+              <v-toolbar color="accent" dark>
+                <v-toolbar-title>Canvas</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>view_module</v-icon>
+                </v-btn>
+              </v-toolbar>
+              <v-card-media>
+                <canvas id="video-canvas" @click="markAdd"></canvas>
+              </v-card-media>
+            </v-card>
+          </v-container>
         </v-flex>
       </v-layout>
     `
