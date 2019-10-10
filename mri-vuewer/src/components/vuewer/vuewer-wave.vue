4673be12177c7a18ba4958f82a5df393b5294ce5
Asai <takuya.waseda.1119@gmail.com>
2019-10-11 00:44:50 +0900

    サブモジュールでは読み込みに不都合があったので ssh で直接読み込むように変更

---
+++ mri-vuewer/src/components/vuewer/vuewer-wave.vue
@@ -0,0 +1,14 @@
+<template> </template>
+
+<script>
+import MultilinePlugin from "../../plugins/wavesurfer/multiline.js";
+export default {
+  props: {
+    src: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style scoped></style>
